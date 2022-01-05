1b2cfa5f8caa150b10f1eeb8e5a7c8ff9dd1d9a0
Bishal Acharya <acharya.bis@gmail.com>
2022-01-05 07:59:03 +0545

    onActivity result done

--- app/src/main/java/com/divyacollege/myapplication/MainActivity.java
+++ app/src/main/java/com/divyacollege/myapplication/MainActivity.java
@@ -1,25 +1,71 @@
 package com.divyacollege.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 int count = 0;
-TextView counter;
+TextView mScoreTv;
 final static String KEY_SCORE = "score";
+final static int REQUEST_CODE = 3;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity","On Pause Called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActvity","On Start Called");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActvity","On Resume Called");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActvity","On Stop Called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActvity","On Destroy Called");
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        
         setContentView(R.layout.activity_main);
-        counter = (TextView)findViewById(R.id.count_tv);
+        mScoreTv = (TextView)findViewById(R.id.count_tv);
         Button countButton = findViewById(R.id.count_button);
         Button submitButton = findViewById(R.id.submit_btn);
         submitButton.setOnClickListener(new View.OnClickListener(){
@@ -28,7 +74,8 @@ final static String KEY_SCORE = "score";
 
                 Intent i = new Intent(MainActivity.this,ScoreActivity.class);
                 i.putExtra(KEY_SCORE,count);
-                startActivity(i);
+//                startActivity(i);
+                startActivityForResult(i,REQUEST_CODE);
             }
         });
         countButton.setOnClickListener(new View.OnClickListener() {
@@ -36,10 +83,26 @@ final static String KEY_SCORE = "score";
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"Button is clicked",Toast.LENGTH_LONG).show();
                 count++;
-                counter.setText(String.valueOf(count));
+                mScoreTv.setText(String.valueOf(count));
             }
         });
 
+
+
+
+
         //TODO do this work
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                count = 0;
+                mScoreTv.setText(String.valueOf(count));
+            }
+        }
+
     }
 }
--- app/src/main/java/com/divyacollege/myapplication/ScoreActivity.java
+++ app/src/main/java/com/divyacollege/myapplication/ScoreActivity.java
@@ -1,7 +1,10 @@
 package com.divyacollege.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,12 +14,23 @@ import static com.divyacollege.myapplication.MainActivity.KEY_SCORE;
 
 public class ScoreActivity extends AppCompatActivity {
     TextView mScoreTv;
+    Button mFinishBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         int score = getIntent().getIntExtra(KEY_SCORE,0);
         mScoreTv = findViewById(R.id.score_tv);
+        mFinishBtn = findViewById(R.id.finishBtn);
+        mFinishBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent replyIntent = new Intent();
+                setResult(RESULT_OK,replyIntent);
+                finish();
+
+            }
+        });
         mScoreTv.setText("Your score is :" + score);
 
     }
